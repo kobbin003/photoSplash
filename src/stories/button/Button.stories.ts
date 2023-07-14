c8681f1e2794c20a32eb90bd0cc1c3ca71212ffb
kobbin003 <dkfeto@gmail.com>
2023-07-14 19:42:06 +0530

    button-stories

---
+++ src/stories/button/Button.stories.ts
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta = {
+	title: "Button",
+	component: Button,
+	tags: ["autodocs"],
+	argTypes: {
+		// backgroundColor: { control: "color" },
+		label: {
+			control: "text",
+			description: "label of the button",
+			type: "string",
+		},
+	},
+	parameters: {
+		backgrounds: {
+			values: [
+				{ name: "blackk", value: "#000" },
+				{ name: "green", value: "green" },
+				{ name: "white", value: "#fff" },
+			],
+		},
+	},
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const onlyLabel: Story = {
+	args: {
+		mode: "only-label",
+		label: "onlylabel",
+		// height: 50,
+		// width: 100,
+	},
+};
+
+export const iconsWithLabel: Story = {
+	args: {
+		mode: "icons-with-label",
+		label: "icons-with-label",
+		imgUrl: "/src/stories/assets/heart.svg",
+		// height: 100,
+		// width: 100,
+	},
+};
+
+export const onlyIcons: Story = {
+	args: {
+		mode: "only-icons",
+		imgUrl: "/src/stories/assets/heart.svg",
+		// height: 100,
+		// width: 100,
+	},
+};
---
+++ src/stories/button/Button.tsx
@@ -0,0 +1,30 @@
+import "./button.css";
+
+interface ButtonProps {
+	mode: "only-icons" | "icons-with-label" | "only-label";
+	label?: string;
+	imgUrl?: string;
+	height?: number;
+	width?: number;
+}
+
+export const Button = ({
+	mode,
+	label,
+	imgUrl,
+	height,
+	width,
+	...props
+}: ButtonProps) => {
+	return (
+		<button
+			type="button"
+			className={`button--${mode}`}
+			style={{ height: `${height}px`, width: `${width}px` }}
+			{...props}
+		>
+			{imgUrl && <img src={imgUrl} />}
+			<span>{label && label}</span>
+		</button>
+	);
+};
