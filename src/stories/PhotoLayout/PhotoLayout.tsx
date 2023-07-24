a8b81ae6c96eb386b10cbe82b084f82783b77b90
kobbin003 <dkfeto@gmail.com>
2023-07-24 16:02:30 +0530

    photoLayout

--- src/stories/PhotoLayout/PhotoLayout.tsx
+++ src/stories/PhotoLayout/PhotoLayout.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { PhotoCard } from "../photoCard/PhotoCard";
 import "./photoLayout.css";
 interface PhotoLayout {
@@ -6,26 +5,68 @@ interface PhotoLayout {
 	height: string;
 	width: string;
 }
+// const PhotoLayout = () => {
 const PhotoLayout = ({ items, height, width }: PhotoLayout) => {
 	return (
 		<div
-			className="photoContainer"
+			className="photoLayoutGridContainer"
 			style={{ height: `${height}`, width: `${width}` }}
+			// className="grid"
 		>
-			{items.map((item) => (
-				<PhotoCard
-					imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
-					height="400px"
-					width="250px"
+			{/* <li className="grid__item">
+				<img
+					className="grid__image"
+					src="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+					alt=""
 				/>
-			))}
-			{items.map((item) => (
-				<PhotoCard
-					imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
-					height="300px"
-					width="auto"
+			</li>
+			<li className="grid__item grid__item--big">
+				<img
+					className="grid__image"
+					src="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+					alt=""
 				/>
-			))}
+			</li>
+			<li className="grid__item grid__item--tall">
+				<img
+					className="grid__image"
+					src="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+					alt=""
+				/>
+			</li>
+			<li className="grid__item grid__item--wide">
+				<img
+					className="grid__image"
+					src="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+					alt=""
+				/>
+			</li> */}
+			<PhotoCard
+				imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+				height="short"
+			/>
+			<PhotoCard
+				imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+				height="tall"
+			/>
+			<PhotoCard
+				imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+				// height="200px"
+				height="medium"
+			/>
+			<PhotoCard
+				imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+				// height="200px"
+				height="normal"
+			/>
+			<PhotoCard
+				imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+				height="short"
+			/>
+			<PhotoCard
+				imgUrl="/src/stories/assets/hooman-r-eDLeSvWI4OI-unsplash.jpg"
+				height="medium"
+			/>
 		</div>
 	);
 };
