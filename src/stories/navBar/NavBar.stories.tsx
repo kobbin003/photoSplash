6455398f5391b78c8fa35ed1b4e59309c9c80636
kobbin003 <dkfeto@gmail.com>
2023-07-29 17:51:33 +0530

    navbar stories

---
+++ src/stories/navBar/NavBar.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import NavBar from "./NavBar";
+import { Button } from "../button/Button";
+const meta = {
+	title: "NavBar",
+	component: NavBar,
+	tags: ["autodocs"],
+	argTypes: {},
+	parameters: {
+		backgrounds: {
+			values: [
+				{ name: "blackk", value: "#000" },
+				{ name: "white", value: "#fff" },
+			],
+		},
+	},
+} satisfies Meta<typeof NavBar>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const NavBarLoggedOut: Story = {
+	args: {
+		mode: "loggedIn",
+		children: (
+			<button id="profile__button">
+				<img src="/src/stories/assets/header/defaultAvatar.svg" />
+			</button>
+		),
+	},
+};
+export const NavBarLoggedIn: Story = {
+	args: {
+		mode: "loggedIn",
+		children: (
+			<Button
+				mode="only-label"
+				label="Login"
+			/>
+		),
+	},
+	// render:(args)=>(<>)
+};
