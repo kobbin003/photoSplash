4a8eccc8b96ea7708a43b74966ff66e5bde354af
kobbin003 <dkfeto@gmail.com>
2023-08-04 12:36:59 +0530

    PhotoGallery

--- src/Components/PhotoGallery/index.tsx
+++ src/Components/PhotoGallery/index.tsx
@@ -3,30 +3,35 @@ import {
 	EditorialPhotosType,
 	ErrorUnsplash,
 	getEditorialPhotos,
-} from "../../utils/queryFunctions/getEditorialPhotos";
+} from "../../utils/queryFunctions/unsplashData/getEditorialPhotos";
 import PhotoLayoutGeneric from "../../stories/PhotoLayout/PhotoLayoutGeneric";
 
 const PhotoGallery = () => {
 	const { isLoading, isError, data, error } = useQuery<
 		EditorialPhotosType[],
 		ErrorUnsplash
-	>({
-		queryKey: ["singlePhoto"],
-		queryFn: getEditorialPhotos,
-	});
+	>(
+		["singlePhoto"],
+		getEditorialPhotos
+		// ,		{ useErrorBoundary: true }
+		//* using useErrorBoundary will propagate the error to the nearest error boundary in the component tree.
+		//* WHICH in this case is "/" i.e <App/>
+		//* BETTER to set error page as below on this component.
+	);
 	if (isLoading) {
 		return <span>Loading...</span>;
 	}
 
 	if (isError) {
-		return <span>Error:{error.error[0]}</span>;
+		console.log("client", error);
+		return <span>Error:{error.errors[0]}</span>;
 	}
-	console.log(data);
+	console.log("photogllery", data);
 	return (
 		<PhotoLayoutGeneric<EditorialPhotosType>
 			items={data}
 			height="auto"
-			width="100vw"
+			width="100%"
 		/>
 	);
 };
