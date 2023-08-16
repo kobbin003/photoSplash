6684642d3afa744c0242cd0d1f278c9da1519d59
kobbin003 <dkfeto@gmail.com>
2023-08-16 11:30:05 +0530

    ModalContainer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import PhotoGallery from "./Components/PhotoGallery";
+import ModalContainer from "./Components/ModalContainer";
 
 function App() {
 	return (
 		<div className="appContainer">
 			<Outlet />
+			<ModalContainer />
 			<PhotoGallery />
 		</div>
 	);
