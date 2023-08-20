157ee6e4c56d3cdaa49ca8bd05ad6704dbef1f94
kobbin003 <dkfeto@gmail.com>
2023-08-20 13:15:59 +0530

    queryFn fetched data types

---
+++ src/utils/queryFunctions/unsplashData/type/EditorialPhotos.ts
@@ -0,0 +1,71 @@
+export interface EditorialPhotosType {
+	id: string;
+	created_at: string;
+	updated_at: string;
+	width: number;
+	height: number;
+	color: string;
+	blur_hash: string;
+	likes: number;
+	liked_by_user: boolean;
+	description: string;
+	user: User;
+	current_user_collections: CurrentUserCollection[];
+	urls: Urls;
+	links: Links2;
+}
+
+export interface User {
+	id: string;
+	username: string;
+	name: string;
+	portfolio_url: string;
+	bio: string;
+	location: string;
+	total_likes: number;
+	total_photos: number;
+	total_collections: number;
+	instagram_username: string;
+	twitter_username: string;
+	profile_image: ProfileImage;
+	links: Links;
+}
+
+export interface ProfileImage {
+	small: string;
+	medium: string;
+	large: string;
+}
+
+export interface Links {
+	self: string;
+	html: string;
+	photos: string;
+	likes: string;
+	portfolio: string;
+}
+
+export interface CurrentUserCollection {
+	id: number;
+	title: string;
+	published_at: string;
+	last_collected_at: string;
+	updated_at: string;
+	cover_photo: any;
+	user: any;
+}
+
+export interface Urls {
+	raw: string;
+	full: string;
+	regular: string;
+	small: string;
+	thumb: string;
+}
+
+export interface Links2 {
+	self: string;
+	html: string;
+	download: string;
+	download_location: string;
+}
---
+++ src/utils/queryFunctions/unsplashData/type/PhotoStats.ts
@@ -0,0 +1,57 @@
+export interface PhotoStats {
+	id: string;
+	downloads: Downloads;
+	views: Views;
+	likes: Likes;
+}
+
+export interface Downloads {
+	total: number;
+	historical: Historical;
+}
+
+export interface Historical {
+	change: number;
+	resolution: string;
+	quantity: number;
+	values: Value[];
+}
+
+export interface Value {
+	date: string;
+	value: number;
+}
+
+export interface Views {
+	total: number;
+	historical: Historical2;
+}
+
+export interface Historical2 {
+	change: number;
+	resolution: string;
+	quantity: number;
+	values: Value2[];
+}
+
+export interface Value2 {
+	date: string;
+	value: number;
+}
+
+export interface Likes {
+	total: number;
+	historical: Historical3;
+}
+
+export interface Historical3 {
+	change: number;
+	resolution: string;
+	quantity: number;
+	values: Value3[];
+}
+
+export interface Value3 {
+	date: string;
+	value: number;
+}
