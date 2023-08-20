016a74bb7089a63fb32bc82b993e6d930db9751b
kobbin003 <dkfeto@gmail.com>
2023-08-20 13:21:31 +0530

    queryKey destructuring modified

--- src/utils/queryFunctions/unsplashData/getEditorialPhotos.ts
+++ src/utils/queryFunctions/unsplashData/getEditorialPhotos.ts
@@ -1,11 +1,14 @@
-const accessKey = import.meta.env.VITE_ACCESS_KEY;
+import { EditorialPhotosType } from "./type/EditorialPhotos";
+import { accessKey } from "./key/accessKey";
+
 export type ErrorUnsplash = {
 	errors: string[];
 };
 export const getEditorialPhotos = async ({ queryKey }: any) => {
-	const [page, ..._] = queryKey.reverse();
-	// console.log("page", page);
-	// return;
+	//* this way of destructuring is not suitable for queries that
+	//* caches the previous datas i.e has {keepPreviousData: true,}
+	// const [page, ..._] = queryKey.reverse();
+	const [_, page] = queryKey;
 	try {
 		const response = await fetch(
 			`https://api.unsplash.com/photos/?client_id=${accessKey}&page=${page}&per_page=10`
@@ -28,75 +31,3 @@ export const getEditorialPhotos = async ({ queryKey }: any) => {
 		return Promise.reject(error);
 	}
 };
-
-export interface EditorialPhotosType {
-	id: string;
-	created_at: string;
-	updated_at: string;
-	width: number;
-	height: number;
-	color: string;
-	blur_hash: string;
-	likes: number;
-	liked_by_user: boolean;
-	description: string;
-	user: User;
-	current_user_collections: CurrentUserCollection[];
-	urls: Urls;
-	links: Links2;
-}
-
-export interface User {
-	id: string;
-	username: string;
-	name: string;
-	portfolio_url: string;
-	bio: string;
-	location: string;
-	total_likes: number;
-	total_photos: number;
-	total_collections: number;
-	instagram_username: string;
-	twitter_username: string;
-	profile_image: ProfileImage;
-	links: Links;
-}
-
-export interface ProfileImage {
-	small: string;
-	medium: string;
-	large: string;
-}
-
-export interface Links {
-	self: string;
-	html: string;
-	photos: string;
-	likes: string;
-	portfolio: string;
-}
-
-export interface CurrentUserCollection {
-	id: number;
-	title: string;
-	published_at: string;
-	last_collected_at: string;
-	updated_at: string;
-	cover_photo: any;
-	user: any;
-}
-
-export interface Urls {
-	raw: string;
-	full: string;
-	regular: string;
-	small: string;
-	thumb: string;
-}
-
-export interface Links2 {
-	self: string;
-	html: string;
-	download: string;
-	download_location: string;
-}
