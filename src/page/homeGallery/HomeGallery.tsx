77b8083fbf5a1f79c4ee18e8ffd3c42977729cac
kobbin003 <dkfeto@gmail.com>
2023-08-22 17:23:38 +0530

    HomeGallery

---
+++ src/page/homeGallery/HomeGallery.tsx
@@ -0,0 +1,13 @@
+import ModalContainer from "../../Components/ModalContainer";
+import PhotoGallery from "../../Components/PhotoGallery";
+
+const HomeGallery = () => {
+	return (
+		<>
+			<ModalContainer />
+			<PhotoGallery />
+		</>
+	);
+};
+
+export default HomeGallery;
