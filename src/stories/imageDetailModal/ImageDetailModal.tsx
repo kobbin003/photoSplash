d5fb34d86d246c14b62471a7bb9ad659d9d7f683
kobbin003 <dkfeto@gmail.com>
2023-08-22 17:30:26 +0530

    ImageDetailModal component decomposition

--- src/stories/imageDetailModal/ImageDetailModal.tsx
+++ src/stories/imageDetailModal/ImageDetailModal.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
 import "./ImageDetailModal.css";
-import { Button } from "../button/Button";
 import { usePhotoStore } from "../../store/store";
-import PhotoStatsOne from "./PhotoStatsOne";
-import PhotoStatsTwo from "./PhotoStatsTwo";
 import { EditorialPhotosType } from "../../utils/queryFunctions/unsplashData/type/EditorialPhotos";
+import Header from "./components/header/Header";
+import PhotoDisplay from "./components/photoDisplay/PhotoDisplay";
+import PhotoStats from "./components/photoStats/PhotoStats";
 interface ImageDetailModalArgs {}
 
 const ImageDetailModal: FC<ImageDetailModalArgs> = () => {
@@ -13,60 +13,14 @@ const ImageDetailModal: FC<ImageDetailModalArgs> = () => {
 		console.log("some error");
 		return;
 	}
+
 	return (
 		<>
 			{currentPhoto && (
 				<div id="modalContent">
-					<div id="header">
-						<div className="userInfo">
-							<a href="">
-								<img
-									src={currentPhoto.user.profile_image.small}
-									height={50}
-									width={50}
-								/>
-							</a>
-							<a href="">{currentPhoto.user.username}</a>
-						</div>
-						<div
-							style={{
-								display: "flex",
-								justifyContent: "center",
-								alignItems: "center",
-							}}
-						>
-							<Button
-								height={35}
-								mode="only-icons"
-								imgUrl="/src/stories/assets/PhotoCard/heart.svg"
-							/>
-							<Button
-								height={35}
-								mode="only-icons"
-								imgUrl="/src/stories/assets/PhotoCard/add.svg"
-							/>
-							<Button
-								mode="only-label"
-								label="Download"
-								height={35}
-							/>
-							<Button
-								height={35}
-								mode="only-icons"
-								imgUrl="/src/stories/assets/modal/downArrow.svg"
-							/>
-						</div>
-					</div>
-					<div id="photo">
-						<img
-							src={currentPhoto.urls.regular}
-							alt=""
-						/>
-					</div>
-					<div id="photoStats">
-						<PhotoStatsOne id={currentPhoto.id} />
-						<PhotoStatsTwo id={currentPhoto.id} />
-					</div>
+					<Header currentPhoto={currentPhoto} />
+					<PhotoDisplay photoUrls={currentPhoto.urls} />
+					<PhotoStats photoId={currentPhoto.id} />
 				</div>
 			)}
 		</>
