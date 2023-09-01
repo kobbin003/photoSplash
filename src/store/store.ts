42f47a5be6e8a431a31c88959a412a60c3565cff
kobbin003 <dkfeto@gmail.com>
2023-09-01 18:06:09 +0530

    showModal, setShowModal added

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,51 @@
 import { create } from "zustand";
-import { EditorialPhotosType } from "../utils/queryFunctions/unsplashData/getEditorialPhotos";
-
+import { devtools, persist } from "zustand/middleware";
+import { EditorialPhotosType } from "../utils/queryFunctions/unsplashData/type/EditorialPhotos";
 interface PhotoStore {
-	currentPhoto: {} | EditorialPhotosType;
-	allPhotos: [] | EditorialPhotosType[];
-	setCurrentPhoto: (val: EditorialPhotosType) => void;
-	setAllPhotos: (val: EditorialPhotosType[]) => void;
+	currentPhoto: null | EditorialPhotosType;
+	currentphotoIndex: null | number;
+	allPhotos: null | EditorialPhotosType[];
+	setCurrentPhoto: (val: EditorialPhotosType | null) => void;
+	setCurrentPhotoIndex: (val: number | undefined) => void;
+	setCurrentPhotoIndexChange: (fn: (prevValue: number) => number) => void;
+	setAllPhotos: (val: EditorialPhotosType[] | null) => void;
+	setAllPhotosPush: (
+		fn: (prevData: EditorialPhotosType[]) => EditorialPhotosType[]
+	) => void;
+	showModal: boolean;
+	setShowModal: (val: boolean) => void;
 }
-export const usePhotoStore = create<PhotoStore>((set) => ({
-	currentPhoto: {},
-	allPhotos: [],
-	setCurrentPhoto: (val: EditorialPhotosType) =>
-		set((state) => ({ ...state, currentPhoto: val })),
-	setAllPhotos: (val: EditorialPhotosType[]) =>
-		set((state) => ({ ...state, allPhotos: val })),
-}));
+export const usePhotoStore = create<PhotoStore>()(
+	devtools(
+		persist(
+			(set) => ({
+				currentPhoto: null,
+				currentphotoIndex: null,
+				allPhotos: null,
+				showModal: false,
+				setCurrentPhoto: (val: EditorialPhotosType | null) =>
+					set((state) => ({ ...state, currentPhoto: val })),
+				setCurrentPhotoIndex: (val: number | undefined) =>
+					set((state) => ({ ...state, currentphotoIndex: val })),
+				setCurrentPhotoIndexChange: (fn: (prevValue: number) => number) =>
+					set((state) => ({
+						...state,
+						currentphotoIndex: fn(state.currentphotoIndex || 0),
+					})),
+				// setCurrentPhotoIndex: (val: number) =>
+				// 	set((state) => ({ ...state, currentphotoIndex: val })),
+				setAllPhotos: (val: EditorialPhotosType[] | null) =>
+					set((state) => ({ ...state, allPhotos: val })),
+				setAllPhotosPush: (
+					fn: (prevData: EditorialPhotosType[]) => EditorialPhotosType[]
+				) =>
+					set((state) => ({ ...state, allPhotos: fn(state.allPhotos || []) })),
+				setShowModal: (val: boolean) =>
+					set((state) => ({ ...state, showModal: val })),
+			}),
+			{
+				name: "photoStore",
+			}
+		)
+	)
+);
