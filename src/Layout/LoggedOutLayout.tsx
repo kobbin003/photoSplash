81df0b94b8fd0d71e1a48617738cb4fb8c3e00ff
kobbin003 <dkfeto@gmail.com>
2023-09-01 18:08:20 +0530

    clean up stored data in PhotoStore

--- src/Layout/LoggedOutLayout.tsx
+++ src/Layout/LoggedOutLayout.tsx
@@ -1,7 +1,17 @@
 import NavBar from "../stories/navBar/NavBar";
 import HomeGallery from "../page/homeGallery/HomeGallery";
 import "../App.css";
+import { usePhotoStore } from "../store/store";
+import { useEffect } from "react";
 const LoggedOutLayout = () => {
+	const { setAllPhotos, setCurrentPhoto, setShowModal } = usePhotoStore();
+	useEffect(() => {
+		// clean up stored data(PhotoStore) on app launch
+		setAllPhotos(null);
+		setCurrentPhoto(null);
+		setShowModal(false);
+	}, []);
+
 	return (
 		<div className="appContainer">
 			<NavBar mode="loggedOut" />
