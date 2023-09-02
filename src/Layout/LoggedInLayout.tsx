b1d40bf32c94a4a31f45cd8a74272a55237ea8b0
kobbin003 <dkfeto@gmail.com>
2023-09-02 15:32:32 +0530

    photostore state set

--- src/Layout/LoggedInLayout.tsx
+++ src/Layout/LoggedInLayout.tsx
@@ -2,9 +2,14 @@ import { usePhotoStore } from "../store/store";
 import NavBar from "../stories/navBar/NavBar";
 import { Outlet } from "react-router-dom";
 import "../App.css";
+import { useEffect } from "react";
 const LoggedInLayout = () => {
-	const photoState = usePhotoStore();
-	// console.log(photoState.currentPhoto?.updated_at);
+	const { setCurrentPhoto, setShowModal } = usePhotoStore();
+	useEffect(() => {
+		// clean up stored data(PhotoStore) on login
+		setCurrentPhoto(null);
+		setShowModal(false);
+	}, []);
 	return (
 		<div className="appContainer">
 			<NavBar mode="loggedIn" />
