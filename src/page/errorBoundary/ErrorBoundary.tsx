7261d9519cb8339ae2f26f35b80a0cff34f88767
kobbin003 <dkfeto@gmail.com>
2023-09-02 15:33:08 +0530

    errorBoundary UI set

--- src/page/errorBoundary/ErrorBoundary.tsx
+++ src/page/errorBoundary/ErrorBoundary.tsx
@@ -1,10 +1,18 @@
 import { useRouteError } from "react-router-dom";
 
 function ErrorBoundary() {
-	let error = useRouteError();
-	console.error("errorBoundary", error);
+	let error = useRouteError() as Error;
+	console.error("errorBoundary");
 	// Uncaught ReferenceError: path is not defined
-	return <div>Hol Up! Wait a minute! something ain't right!</div>;
+	return (
+		<div>
+			<h3>Hol Up! Wait a minute! something ain't right!</h3>
+			<h5>
+				{error.name}: {error.message}
+			</h5>
+			{/* <p>{error.stack} </p> */}
+		</div>
+	);
 }
 
 export default ErrorBoundary;
