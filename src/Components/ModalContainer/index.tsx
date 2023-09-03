72d96c341a3ed60cdee92ab440af2b255b0ed901
kobbin003 <dkfeto@gmail.com>
2023-09-03 16:33:52 +0530

    forwardRef used for useClickedOutside hook

--- src/Components/ModalContainer/index.tsx
+++ src/Components/ModalContainer/index.tsx
@@ -1,61 +1,125 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import "./style.css";
 import ImageDetailModal from "../../stories/imageDetailModal/ImageDetailModal";
+import { usePhotoStore } from "../../store/store";
+import { useQuery } from "@tanstack/react-query";
+import { EditorialPhotosType } from "../../utils/queryFunctions/unsplashData/type/EditorialPhotos";
+import {
+	ErrorUnsplash,
+	getEditorialPhotos,
+} from "../../utils/queryFunctions/unsplashData/getEditorialPhotos";
+import LeftOfModal from "./LeftOfModal";
+import RightOfModal from "./rightOfModal/RightOfModal";
+import {
+	// useClickedOutsideId,
+	useClickedOutsideRef,
+} from "../../hooks/useClickedOutside";
+
+type ZoomContext = {
+	zoom: boolean;
+	setZoom: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export const ZoomContext = createContext<ZoomContext | null>(null);
 const ModalContainer = () => {
-	const [showModal, setShowModal] = useState(false);
-	const handleCloseModal = () => {
-		document.body.style.overflow = "scroll";
+	const [zoom, setZoom] = useState(false);
+	const { allPhotos, currentPhoto, setAllPhotosPush, setShowModal } =
+		usePhotoStore();
+
+	const [disableNextButton, setDisableNextButton] = useState(false);
+	const [page, setPage] = useState<number | undefined>();
+	const [lastIndex, setLastIndex] = useState<number | undefined>();
+	const [limitExceeded, setLimitExceeded] = useState<boolean>(false);
+	const [fetchPhoto, setFetchPhoto] = useState(false);
+	const { isLoading, data, error } = useQuery<
+		{ photos: EditorialPhotosType[]; remainingLimit: string | null },
+		ErrorUnsplash
+	>(["singlePhotomodal", page], getEditorialPhotos, {
+		keepPreviousData: true,
+		useErrorBoundary: false,
+		enabled: limitExceeded || fetchPhoto, // pause it if limitExceeded or !fetchPhoto
+	});
+
+	const imageDetailModalRef = useRef<HTMLDivElement>(null);
+	const prevButtonRef = useRef<HTMLImageElement>(null);
+	const nextButtonRef = useRef<HTMLImageElement>(null);
+
+	/** using useClickedOutsideRef */
+	const reffs = [imageDetailModalRef, prevButtonRef, nextButtonRef];
+	useClickedOutsideRef(reffs, () => {
 		setShowModal(false);
-	};
+	});
+
+	/** using useClickedOutsideId */
+	// const ids = ["modalContent", "nav-btn_right", "nav-btn_left"];
+	// useClickedOutsideId(ids, () => {
+	// 	setShowModal(false);
+	// });
+	//* fetch only if the photo clicked is the last photo in allPhotos
 	useEffect(() => {
-		const handleShowModal = () => {
-			document.body.style.overflow = "hidden";
-			setShowModal(true);
-		};
-		window.addEventListener("showModal", handleShowModal);
-		return () => window.removeEventListener("showModal", handleShowModal);
+		if (allPhotos) {
+			const currentPhotoIndex = allPhotos.findIndex(
+				(photo) => photo.id == currentPhoto?.id
+			);
+			if (currentPhotoIndex == allPhotos?.length - 1) {
+				// console.log(currentPhoto?.page);
+				setPage(currentPhotoIndex + 1);
+				setFetchPhoto(true);
+			}
+		}
 	}, []);
+
+	//* disable nextButton if data is loading
+	useEffect(() => {
+		if (fetchPhoto && isLoading) {
+			setDisableNextButton(true);
+		} else {
+			setDisableNextButton(false);
+		}
+	}, [isLoading]);
+
+	//* update allPhotos on data fetch
+	useEffect(() => {
+		if (data) {
+			setAllPhotosPush((prev) => [...prev, ...data.photos]);
+			//* do the following if limit exceed condition is set
+			if (Number(data.remainingLimit) < 5) {
+				console.log(" limit has been exceeded");
+				setLimitExceeded(true);
+				setDisableNextButton(true);
+				allPhotos && setLastIndex(allPhotos?.length - 1);
+			}
+		}
+		// console.log("remaining limit", data?.remainingLimit);
+	}, [data]);
+
 	return (
-		<div
-			style={{
-				display: showModal ? "flex" : "none",
-				justifyContent: "center",
-			}}
-		>
-			/** layer-1: the black curtain */
+		<div id="modalContainer">
 			<div id="blackCurtain"></div>
-			/** layer-2: the ImageDetailModal */
-			<div id="modalContainer">
-				<ImageDetailModal />
+			<div id="modalCompContainer">
+				<ZoomContext.Provider value={{ zoom, setZoom }}>
+					{!error ? (
+						<ImageDetailModal ref={imageDetailModalRef} />
+					) : (
+						<p>{error.errors}</p>
+					)}
+				</ZoomContext.Provider>
 			</div>
-			/** layer-3: the nav & close buttons */
-			<div className="blackSpace blackSpace__left">
-				<button
-					id="close"
-					onClick={handleCloseModal}
-				>
-					<img
-						src="/src/assets/close.svg"
-						alt="close"
-						height={25}
-						width={25}
-					/>
-				</button>
-				<button>
-					<img
-						src="/src/stories/assets/imageModal/leftArrow.svg"
-						alt="left"
-					/>
-				</button>
-			</div>
-			<div className="blackSpace blackSpace__right">
-				<button>
-					<img
-						src="/src/stories/assets/imageModal/rightArrow.svg"
-						alt="right"
-					/>
-				</button>
-			</div>
+			<LeftOfModal
+				ref={prevButtonRef}
+				zoom={zoom}
+				disableNextButton={disableNextButton}
+				setDisableNextButton={setDisableNextButton}
+			/>
+			<RightOfModal
+				ref={nextButtonRef}
+				zoom={zoom}
+				lastIndex={lastIndex}
+				limitExceeded={limitExceeded}
+				setPage={setPage}
+				setFetchPhoto={setFetchPhoto}
+				disableNextButton={disableNextButton}
+				setDisableNextButton={setDisableNextButton}
+			/>
 		</div>
 	);
 };
--- src/stories/imageDetailModal/ImageDetailModal.tsx
+++ src/stories/imageDetailModal/ImageDetailModal.tsx
@@ -1,30 +1,60 @@
-import { FC } from "react";
+import { ForwardRefRenderFunction, forwardRef } from "react";
 import "./ImageDetailModal.css";
 import { usePhotoStore } from "../../store/store";
-import { EditorialPhotosType } from "../../utils/queryFunctions/unsplashData/type/EditorialPhotos";
 import Header from "./components/header/Header";
 import PhotoDisplay from "./components/photoDisplay/PhotoDisplay";
 import PhotoStats from "./components/photoStats/PhotoStats";
 interface ImageDetailModalArgs {}
 
-const ImageDetailModal: FC<ImageDetailModalArgs> = () => {
-	const currentPhoto = usePhotoStore().currentPhoto as EditorialPhotosType;
+const ImageDetailModal: ForwardRefRenderFunction<
+	HTMLDivElement,
+	ImageDetailModalArgs
+> = (_, ref) => {
+	const { currentPhoto } = usePhotoStore();
 	if (!currentPhoto) {
 		console.log("some error");
-		return;
+		return <div>Loading..</div>;
 	}
 
 	return (
 		<>
-			{currentPhoto && (
-				<div id="modalContent">
-					<Header currentPhoto={currentPhoto} />
-					<PhotoDisplay photoUrls={currentPhoto.urls} />
-					<PhotoStats photoId={currentPhoto.id} />
-				</div>
-			)}
+			<div
+				id="modalContent"
+				ref={ref}
+			>
+				<Header currentPhoto={currentPhoto} />
+				<PhotoDisplay photoUrls={currentPhoto.urls} />
+				<PhotoStats photoId={currentPhoto.id} />
+			</div>
 		</>
 	);
 };
 
-export default ImageDetailModal;
+export default forwardRef(ImageDetailModal);
+
+/** ********************* Another approach *************** */
+// const ImageDetailModal = forwardRef<HTMLDivElement, ImageDetailModalArgs>(
+// 	(props, ref) => {
+// 		const { currentPhoto } = usePhotoStore();
+// 		if (!currentPhoto) {
+// 			console.log("some error");
+// 			return <div>Loading..</div>;
+// 		}
+
+// 		// console.log("ZOOM--context", context);
+// 		return (
+// 			<>
+// 				<div
+// 					id="modalContent"
+// 					ref={ref}
+// 					// style={{ maxHeight: zoom ? "100vh" : "none" }}
+// 				>
+// 					<Header currentPhoto={currentPhoto} />
+// 					<PhotoDisplay photoUrls={currentPhoto.urls} />
+// 					<PhotoStats photoId={currentPhoto.id} />
+// 				</div>
+// 			</>
+// 		);
+// 	}
+// );
+// export default ImageDetailModal;
