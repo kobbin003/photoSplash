904dfbe4b13a15c07b1a620acc252aec23d369b4
kobbin003 <dkfeto@gmail.com>
2023-09-03 16:19:54 +0530

    currentPhotoIndex & setCurrentPhotoIndex props removed

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,8 @@ import { devtools, persist } from "zustand/middleware";
 import { EditorialPhotosType } from "../utils/queryFunctions/unsplashData/type/EditorialPhotos";
 interface PhotoStore {
 	currentPhoto: null | EditorialPhotosType;
-	currentphotoIndex: null | number;
-	allPhotos: null | EditorialPhotosType[];
 	setCurrentPhoto: (val: EditorialPhotosType | null) => void;
-	setCurrentPhotoIndex: (val: number | undefined) => void;
-	setCurrentPhotoIndexChange: (fn: (prevValue: number) => number) => void;
+	allPhotos: null | EditorialPhotosType[];
 	setAllPhotos: (val: EditorialPhotosType[] | null) => void;
 	setAllPhotosPush: (
 		fn: (prevData: EditorialPhotosType[]) => EditorialPhotosType[]
@@ -20,26 +17,16 @@ export const usePhotoStore = create<PhotoStore>()(
 		persist(
 			(set) => ({
 				currentPhoto: null,
-				currentphotoIndex: null,
-				allPhotos: null,
-				showModal: false,
 				setCurrentPhoto: (val: EditorialPhotosType | null) =>
 					set((state) => ({ ...state, currentPhoto: val })),
-				setCurrentPhotoIndex: (val: number | undefined) =>
-					set((state) => ({ ...state, currentphotoIndex: val })),
-				setCurrentPhotoIndexChange: (fn: (prevValue: number) => number) =>
-					set((state) => ({
-						...state,
-						currentphotoIndex: fn(state.currentphotoIndex || 0),
-					})),
-				// setCurrentPhotoIndex: (val: number) =>
-				// 	set((state) => ({ ...state, currentphotoIndex: val })),
+				allPhotos: null,
 				setAllPhotos: (val: EditorialPhotosType[] | null) =>
 					set((state) => ({ ...state, allPhotos: val })),
 				setAllPhotosPush: (
 					fn: (prevData: EditorialPhotosType[]) => EditorialPhotosType[]
 				) =>
 					set((state) => ({ ...state, allPhotos: fn(state.allPhotos || []) })),
+				showModal: false,
 				setShowModal: (val: boolean) =>
 					set((state) => ({ ...state, showModal: val })),
 			}),
