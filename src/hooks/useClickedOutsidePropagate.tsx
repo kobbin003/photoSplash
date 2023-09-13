15bd03204b25d74b022a80fade87bdf1c0ed9521
kobbin003 <dkfeto@gmail.com>
2023-09-13 13:34:49 +0530

    useClickedOutsidePropagate hook

---
+++ src/hooks/useClickedOutsidePropagate.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const useOutsideClickPropagate = (callback: () => void) => {
+	const handleClick = (e: any) => {
+		/** do stopPropagation in the click event on the clicked element. */
+		callback();
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", handleClick);
+
+		return () => {
+			document.removeEventListener("click", handleClick);
+		};
+	}, []);
+};
+
+export default useOutsideClickPropagate;
