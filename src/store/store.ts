321e18399161a25e7491ab1576d337378a10951b
kobbin003 <dkfeto@gmail.com>
2023-09-15 11:10:00 +0530

    currentUser & setCurrentUser added

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { EditorialPhotosType } from "../utils/queryFunctions/unsplashData/type/EditorialPhotos";
+import { CurrentUserProfile } from "../utils/queryFunctions/unsplashData/type/CurrentUserProfile";
 interface PhotoStore {
 	currentPhoto: null | EditorialPhotosType;
 	setCurrentPhoto: (val: EditorialPhotosType | null) => void;
@@ -11,6 +12,10 @@ interface PhotoStore {
 	) => void;
 	showModal: boolean;
 	setShowModal: (val: boolean) => void;
+	accessToken: string;
+	setAccessToken: (val: string) => void;
+	currentUserProfile: null | CurrentUserProfile;
+	setCurrentUserProfile: (val: CurrentUserProfile) => void;
 }
 export const usePhotoStore = create<PhotoStore>()(
 	devtools(
@@ -29,6 +34,12 @@ export const usePhotoStore = create<PhotoStore>()(
 				showModal: false,
 				setShowModal: (val: boolean) =>
 					set((state) => ({ ...state, showModal: val })),
+				accessToken: "",
+				setAccessToken: (val) =>
+					set((state) => ({ ...state, accessToken: val })),
+				currentUserProfile: null,
+				setCurrentUserProfile: (val) =>
+					set((state) => ({ ...state, currentUserProfile: val })),
 			}),
 			{
 				name: "photoStore",
