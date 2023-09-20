26c778ccb1e9675ae834bd8086223ebb7a1fdf34
kobbin003 <dkfeto@gmail.com>
2023-09-20 13:02:20 +0530

    profile routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,30 +1,114 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import { NavBarLoggedOut } from "./stories/navBar/NavBar.stories";
-import NavBar from "./stories/navBar/NavBar";
 import LogIn from "./page/logIn";
+import LoggedOutLayout from "./Layout/LoggedOutLayout";
+import { Suspense, lazy } from "react";
+const LoggedInLayout = lazy(() => import("./Layout/LoggedInLayout"));
+import HomeGallery from "./page/homeGallery/HomeGallery";
 import ErrorBoundary from "./page/errorBoundary/ErrorBoundary";
-
+import Profile from "./page/profile/Profile";
+import Photos from "./page/profile/photos/Photos";
+import Stats from "./page/profile/stats/Stats";
+import Likes from "./page/profile/likes/Likes";
+import Collections from "./page/profile/collections/Collections";
 export const router = createBrowserRouter([
 	{
 		path: "*",
 		element: <p>404: NOT FOUND</p>,
 	},
+
 	{
 		path: "/",
-		element: <App />,
+		element: <LoggedOutLayout />,
 		errorElement: <ErrorBoundary />,
-		// errorElement: <p>ERROR APP:</p>,
 		children: [
 			{
 				index: true,
-				element: <NavBar mode="loggedOut" />,
+				element: <HomeGallery />,
 			},
 			{
-				path: "me",
-				element: <NavBar mode="loggedIn" />,
+				path: "profile/:userName",
+				element: <Profile />,
+				children: [
+					{
+						index: true,
+						element: <Photos />,
+					},
+					{
+						path: "likes",
+						element: <Likes />,
+					},
+					{
+						path: "collections",
+						element: <Collections />,
+					},
+					{
+						path: "stats",
+						element: <Stats />,
+					},
+				],
 			},
 		],
 	},
+
+	{
+		path: "/me",
+		element: (
+			<Suspense fallback={<p>Loading...</p>}>
+				<LoggedInLayout />
+			</Suspense>
+		),
+		errorElement: <ErrorBoundary />,
+		children: [
+			{
+				index: true,
+				element: <HomeGallery />,
+			},
+			{
+				path: "profile",
+				element: <Profile />,
+				children: [
+					{
+						index: true,
+						element: <Photos />,
+					},
+					{
+						path: "likes",
+						element: <Likes />,
+					},
+					{
+						path: "collections",
+						element: <Collections />,
+					},
+					{
+						path: "stats",
+						element: <Stats />,
+					},
+				],
+			},
+			{
+				path: "profile/:userName",
+				element: <Profile />,
+				children: [
+					{
+						index: true,
+						element: <Photos />,
+					},
+					{
+						path: "likes",
+						element: <Likes />,
+					},
+					{
+						path: "collections",
+						element: <Collections />,
+					},
+					{
+						path: "stats",
+						element: <Stats />,
+					},
+				],
+			},
+		],
+	},
+
 	{ path: "/callback", element: <LogIn /> },
 ]);
