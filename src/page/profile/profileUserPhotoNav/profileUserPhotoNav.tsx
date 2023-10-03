66756f3dfead2f87bf677362996d216ef851540c
kobbin003 <dkfeto@gmail.com>
2023-10-03 17:19:40 +0530

    profile nav links corrected

--- src/page/profile/profileUserPhotoNav/profileUserPhotoNav.tsx
+++ src/page/profile/profileUserPhotoNav/profileUserPhotoNav.tsx
@@ -1,67 +1,111 @@
 import { Link, useLocation, useParams } from "react-router-dom";
 import "./style.css";
-import { MouseEvent, useEffect, useState } from "react";
-const ProfileUserPhotoNav = () => {
-	const { userId } = useParams();
+import { FC, MouseEvent, useEffect, useState } from "react";
+import { capitalizeString } from "../../../utils/capitalizeString";
 
-	const { state } = useLocation();
+type Props = {
+	photosCount: number;
+	likesCount: number;
+	collectionsCount: number;
+	loggedIn: boolean;
+};
 
-	const [currentAnchor, setCurrentAnchor] = useState(state.show);
+const ProfileUserPhotoNav: FC<Props> = ({
+	photosCount,
+	likesCount,
+	collectionsCount,
+	loggedIn,
+}) => {
+	const { userName } = useParams();
+
+	const { state, pathname } = useLocation();
+
+	const pathSplitted = pathname.split("/");
+	const pathSlug = pathSplitted[pathSplitted.length - 1];
+	const pathSlugCapitalized = capitalizeString(pathSlug);
+
+	const [currentAnchor, setCurrentAnchor] = useState<string>();
 
 	const handleSetBorderBottom = (e: MouseEvent<HTMLAnchorElement>) => {
 		const clickedEl = e.currentTarget as HTMLElement;
-		setCurrentAnchor(clickedEl.innerText);
+		const firstSpan = clickedEl.querySelector("span");
+		firstSpan?.innerText && setCurrentAnchor(firstSpan?.innerText);
 	};
 
 	useEffect(() => {
-		setCurrentAnchor(state.show);
-	}, [state.show]);
+		if (state) {
+			// console.log("state", state);
+			setCurrentAnchor(state.show);
+		} else if (
+			["Likes", "Collections", "Stats"].includes(pathSlugCapitalized)
+		) {
+			setCurrentAnchor(pathSlugCapitalized);
+			// console.log("slug", pathSlugCapitalized);
+		} else {
+			// console.log("slug", "Photos");
+			setCurrentAnchor("Photos");
+		}
+	}, [state]);
 
 	return (
 		<div id="ProfileUserPhotoNav">
 			<Link
-				to={userId ? `/${userId}/profile` : "/me/profile"}
+				to={!loggedIn ? `/profile/${userName}` : `/me/profile/${userName}`}
 				onClick={handleSetBorderBottom}
 				className={
 					currentAnchor == "Photos" ? "current" : "transparentBorderbottom"
 				}
-				// style={{ borderBottom: "3px solid black" }}
 			>
-				<img
-					src="/src/assets/photo3.svg"
-					alt=""
-				/>
+				<div>
+					<img
+						src="/src/assets/photo3.svg"
+						alt=""
+					/>
+				</div>
 				<span>Photos</span>
+				<span>{photosCount || 0}</span>
 			</Link>
 			<Link
-				to={userId ? `/${userId}/profile/likes` : "/me/profile/likes"}
+				to={
+					!loggedIn
+						? `/profile/${userName}/likes`
+						: `/me/profile/${userName}/likes`
+				}
 				onClick={handleSetBorderBottom}
 				className={
 					currentAnchor == "Likes" ? "current" : "transparentBorderbottom"
 				}
 			>
-				<img
-					src="/src/assets/like.svg"
-					alt=""
-				/>
+				<div>
+					<img
+						src="/src/assets/like.svg"
+						alt=""
+					/>
+				</div>
 				<span>Likes</span>
+				<span>{likesCount || 0}</span>
 			</Link>
 			<Link
 				to={
-					userId ? `/${userId}/profile/collections` : "/me/profile/collections"
+					!loggedIn
+						? `/profile/${userName}/collections`
+						: `/me/profile/${userName}/collections`
 				}
 				onClick={handleSetBorderBottom}
 				className={
 					currentAnchor == "Collections" ? "current" : "transparentBorderbottom"
 				}
 			>
-				<img
-					src="/src/assets/photocollection3.svg"
-					alt=""
-				/>
+				<div>
+					<img
+						src="/src/assets/photocollection3.svg"
+						alt=""
+					/>
+				</div>
 				<span>Collections</span>
+				<span>{collectionsCount || 0}</span>
 			</Link>
-			{!userId && (
+			{!userName && (
 				<Link
 					to={"/me/profile/stats"}
 					onClick={handleSetBorderBottom}
@@ -69,10 +113,12 @@ const ProfileUserPhotoNav = () => {
 						currentAnchor == "Stats" ? "current" : "transparentBorderbottom"
 					}
 				>
-					<img
-						src="/src/assets/photocollection3.svg"
-						alt=""
-					/>
+					<div>
+						<img
+							src="/src/assets/photocollection3.svg"
+							alt=""
+						/>
+					</div>
 					<span>Stats</span>
 				</Link>
 			)}
