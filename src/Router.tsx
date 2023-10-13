ca0f99ccff766eb7363d26f8d31662b39acb42ff
kobbin003 <dkfeto@gmail.com>
2023-10-13 20:42:52 +0530

    search route added

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import LogIn from "./page/logIn";
 import LoggedOutLayout from "./Layout/LoggedOutLayout";
 import { Suspense, lazy } from "react";
 const LoggedInLayout = lazy(() => import("./Layout/LoggedInLayout"));
@@ -11,7 +10,55 @@ import Stats from "./page/profile/stats/Stats";
 import Likes from "./page/profile/likes/Likes";
 import Collections from "./page/profile/collections/Collections";
 import CollectionPage from "./page/profile/collections/collectionPage/CollectionPage";
+import LogIn from "./page/logIn";
+import Search from "./page/search/Search";
+import PhotosSearch from "./page/search/photos/PhotosSearch";
+import CollectionsSearch from "./page/search/collections/CollectionsSearch";
+import UsersSearch from "./page/search/users/UsersSearch";
 // import HomeGalleryInfinite from "./page/homeGallery/HomeGalleryInfinite";
+
+const children = [
+	{
+		index: true,
+		element: <HomeGallery />,
+	},
+	{
+		path: "search/:searchQuery",
+		element: <Search />,
+		children: [
+			{ path: "photos", element: <PhotosSearch /> },
+			{ path: "collections", element: <CollectionsSearch /> },
+			{ path: "users", element: <UsersSearch /> },
+		],
+	},
+	{
+		path: "profile/:userName",
+		element: <Profile />,
+		children: [
+			{
+				index: true,
+				element: <Photos />,
+			},
+			{
+				path: "likes",
+				element: <Likes />,
+			},
+			{
+				path: "collections",
+				element: <Collections />,
+			},
+
+			{
+				path: "stats",
+				element: <Stats />,
+			},
+		],
+	},
+	{
+		path: "profile/collections/:collectionId",
+		element: <CollectionPage />,
+	},
+];
 
 export const router = createBrowserRouter([
 	{
@@ -23,40 +70,7 @@ export const router = createBrowserRouter([
 		path: "/",
 		element: <LoggedOutLayout />,
 		errorElement: <ErrorBoundary />,
-		children: [
-			{
-				index: true,
-				element: <HomeGallery />,
-				// element: <HomeGalleryInfinite />,
-			},
-			{
-				path: "profile/:userName",
-				element: <Profile />,
-				children: [
-					{
-						index: true,
-						element: <Photos />,
-					},
-					{
-						path: "likes",
-						element: <Likes />,
-					},
-					{
-						path: "collections",
-						element: <Collections />,
-					},
-
-					{
-						path: "stats",
-						element: <Stats />,
-					},
-				],
-			},
-			{
-				path: "/profile/collections/:collectionId",
-				element: <CollectionPage />,
-			},
-		],
+		children,
 	},
 
 	{
@@ -67,56 +81,7 @@ export const router = createBrowserRouter([
 			</Suspense>
 		),
 		errorElement: <ErrorBoundary />,
-		children: [
-			{
-				index: true,
-				element: <HomeGallery />,
-			},
-			{
-				path: "profile",
-				element: <Profile />,
-				children: [
-					{
-						index: true,
-						element: <Photos />,
-					},
-					{
-						path: "likes",
-						element: <Likes />,
-					},
-					{
-						path: "collections",
-						element: <Collections />,
-					},
-					{
-						path: "stats",
-						element: <Stats />,
-					},
-				],
-			},
-			{
-				path: "profile/:userName",
-				element: <Profile />,
-				children: [
-					{
-						index: true,
-						element: <Photos />,
-					},
-					{
-						path: "likes",
-						element: <Likes />,
-					},
-					{
-						path: "collections",
-						element: <Collections />,
-					},
-					{
-						path: "stats",
-						element: <Stats />,
-					},
-				],
-			},
-		],
+		children,
 	},
 
 	{ path: "/callback", element: <LogIn /> },
