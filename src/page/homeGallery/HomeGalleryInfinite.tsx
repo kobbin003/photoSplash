6775a29db7e90c602412ea393dfe147f5bc0cf2e
kobbin003 <dkfeto@gmail.com>
2023-10-13 13:40:10 +0530

    HomeGallery for useInfintequery

---
+++ src/page/homeGallery/HomeGalleryInfinite.tsx
@@ -0,0 +1,12 @@
+import PhotoGalleryInfinite from "../../Components/PhotoGallery/indexInfinite";
+
+const HomeGalleryInfinite = () => {
+	console.log("photoGallery-infinite");
+	return (
+		<>
+			<PhotoGalleryInfinite />
+		</>
+	);
+};
+
+export default HomeGalleryInfinite;
